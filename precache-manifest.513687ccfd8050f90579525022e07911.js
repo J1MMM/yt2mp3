da1df7a67453eeb9afabeb3e3971edb9f4a8ad70
J1MMM <jimuelbaraero00@gmail.com>
2022-07-24 17:41:26 +0800

    Updates

---
+++ precache-manifest.513687ccfd8050f90579525022e07911.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee637bd4f7b486cf4984",
+    "url": "/yt2mp3/static/css/main.aa45f371.chunk.css"
+  },
+  {
+    "revision": "ee637bd4f7b486cf4984",
+    "url": "/yt2mp3/static/js/main.ee637bd4.chunk.js"
+  },
+  {
+    "revision": "87dd06f5dc47edc8092b",
+    "url": "/yt2mp3/static/js/1.87dd06f5.chunk.js"
+  },
+  {
+    "revision": "0ac7c4424e3c6fae53ee",
+    "url": "/yt2mp3/static/js/runtime~main.0ac7c442.js"
+  },
+  {
+    "revision": "651940775d7e0cc236fda9c13a6f9a42",
+    "url": "/yt2mp3/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/yt2mp3/precache-manifest.7f79e739b756ba6dece7eb2d69573b14.js"
+  "/yt2mp3/precache-manifest.513687ccfd8050f90579525022e07911.js"
 );
 
 workbox.clientsClaim();
