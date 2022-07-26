b73293d2afd6e198b479747ccf7a30150e1fa257
J1MMM <jimuelbaraero00@gmail.com>
2022-07-26 14:17:31 +0800

    footer update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,11 @@
 import React from "react";
 import '../style/variables.css'
-
+import '../style/Footer.css'
 export default function Footer(){
     const style = {
         backgroundColor: 'var(--bg-dark)',
-        color: '#fff',
+        color: 'black',
         width: '100%',
-        height: '3rem',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
@@ -14,8 +13,18 @@ export default function Footer(){
         marginTop: "1rem"
 }
     return(
-        <div style={style} className="Footer">
-            <h3>Footer</h3>
+        <div style={style} className="footer-container">
+            <div className="footer-items">
+                <a href="https://www.facebook.com/jimuel.baraero" target='_blank'>
+                <img src='https://cdn-icons-png.flaticon.com/512/20/20673.png' atl='' />
+                </a>
+                <a href="https://github.com/J1MMM" target='_blank'>
+                <img src='https://cdn-icons-png.flaticon.com/512/25/25231.png' atl='' />
+                </a>
+                <a href="https://www.linkedin.com/in/jimuel-baraero-b49628243" target='_blank'>
+                <img src='https://cdn-icons-png.flaticon.com/512/61/61109.png' atl='' />
+                </a>
+            </div>
         </div>
     )
 }
